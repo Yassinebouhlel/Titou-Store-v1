99b067b826724c789cca8e09c87dfe52bfa8cd85
Yassinebouhlel <yassinebouhlel.by@gmail.com>
2024-11-07 00:45:04 -0500

    cleanup 2

--- src/app/[store]/contact/page.tsx
+++ src/app/[store]/contact/page.tsx
@@ -100,7 +100,7 @@ export default function ProductPage() {
               <textarea
                 id="message"
                 name="message"
-                rows="5"
+                // rows="5"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
                 placeholder={f('Message')}
                 value={formData.message}
